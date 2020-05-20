b634b4eefcc65eee40d2eb4f5a8526662fb86bb8
shaden <lucasian-shaden@lucasianshaden>
2020-05-19 20:05:48 -0500

    implementación de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+// Modulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
@@ -10,6 +12,10 @@ import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +30,8 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     BreadcrumbsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
